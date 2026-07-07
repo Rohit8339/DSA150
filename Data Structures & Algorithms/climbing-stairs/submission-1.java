0f6ad989a245219ce4f0908e44e08f3cc041ea47
Rohit8339 <115556516+Rohit8339@users.noreply.github.com>
2026-07-07 13:15:11 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return f(0, n,  dp);
+        
+    }
+
+    int f(int i, int n, int dp[])  {
+        if(i == n) return 1;
+        if(i > n ) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        return dp[i] = f(i + 1, n, dp) + f(i + 2, n, dp);
+    }
+}
